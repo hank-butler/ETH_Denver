58ec76b754f86f8cfe0f11c9bcfab46f8316afa0
Hank <hank.butler@protonmail.com>
2022-11-11 22:42:33 -0700

    Update HelloWorld.ts

--- project/tests/HelloWorld.ts
+++ project/tests/HelloWorld.ts
@@ -1,6 +1,5 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { HelloWorld } from "../typechain-types";
 
 describe("Hello World", () => {
     it("should give a Hello World message", async () => {
@@ -9,5 +8,15 @@ describe("Hello World", () => {
         await helloWorldContract.deployed();
         const helloWorldText = await helloWorldContract.helloWorld();
         expect(helloWorldText).to.eq("Hello World"); 
+    });
+    
+    it("should set the owner as the deployer", async () => {
+        const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
+        const helloWorldContract = await helloWorldFactory.deploy();
+        await helloWorldContract.deployed();
+        const helloWorldText = await helloWorldContract.helloWorld();
+        const accounts = await ethers.getSigners();
+        const contractOwner = await helloWorldContract.owner();
+        expect(contractOwner).to.eq(accounts[0].address)
     });
 });
