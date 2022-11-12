f3bf78b0cfae898e4561d51bb725def169d6beff
Hank <hank.butler@protonmail.com>
2022-11-11 22:38:00 -0700

    successfully ran unit test

--- project/tests/HelloWorld.ts
+++ project/tests/HelloWorld.ts
@@ -1,16 +1,13 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { HelloWorld } from "../typechain-types";
 
 describe("Hello World", () => {
     it("should give a Hello World message", async () => {
-        // Let's just do some exploration
-        const signers = await ethers.getSigners();
-        signers.forEach(async (signer) => {
-            console.log(`This signer has the address of: ${signer.address}`)
-            const balance = await signer.getBalance();
-            console.log(`This signer has the balance of: ${balance}`)
-        })
-
-        expect(false).to.eq(true); 
+        const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
+        const helloWorldContract = await helloWorldFactory.deploy();
+        await helloWorldContract.deployed();
+        const helloWorldText = await helloWorldContract.helloWorld();
+        expect(helloWorldText).to.eq("Hello World"); 
     });
 });
